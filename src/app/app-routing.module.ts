38f71064879eac32b45863986fdf438778f20504
bayrom <ba.caamano@duocuc.cl>
2024-08-16 13:44:00 -0400

    segunda parte

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+import { ServiciosComponent } from './pages/servicios/servicios.component';
+import { NotfoundComponent } from './pages/notfound/notfound.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component:HomeComponent
+  },
+
+  {
+    path:'contacto',
+    component:ContactoComponent
+  },
+
+  {
+    path:'servicios',
+    component:ServiciosComponent
+  },
+
+  {
+    path:'**',
+    component:NotfoundComponent
+  },
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+import { ServiciosComponent } from './pages/servicios/servicios.component';
+import { NotfoundComponent } from './pages/notfound/notfound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactoComponent,
+    ServiciosComponent,
+    NotfoundComponent,
   ],
   imports: [
     BrowserModule,
